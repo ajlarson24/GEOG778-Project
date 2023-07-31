32d562b5aa7265c6deed0772d88a1a457945d91e
ajlarson24 <123599236+ajlarson24@users.noreply.github.com>
2023-07-31 15:19:09 -0500

    7/31 Work
    Added design elements, popup, and restyled map

--- js/main.js
+++ js/main.js
@@ -1,18 +1,55 @@
-/* Map of GeoJSON data from MegaCities.geojson */
+/* SHARKVIZ SOURCE CODE */
 //declare map var in global scope
 var map;
+
+//initial popup window
+window.addEventListener("load", function () {
+    this.setTimeout(
+        function open(event) {
+            document.querySelector(".popup").style.display = "block";
+        },
+        0
+    )
+});
+
+//popup window interactions
+document.querySelector("#close").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "none";
+});
+
+document.querySelector("#letsGo").addEventListener("click", function () {
+    document.querySelector(".popup").style.display = "none";
+});
+
 //function to instantiate the Leaflet map
 function createMap(){
     //create the map
     map = L.map('map', {
         center: [36, -72.5],
-        zoom: 5
+        zoom: 5,
+        zoomControl: false
     });
 
     //add OSM base tilelayer
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	    maxZoom: 19,
+	    attribution: 'Chris Fischer and the &copy <a href="https://www.ocearch.org/tracker/">Ocearch</a> Team, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+    L.control.zoom({
+        position: 'bottomright'
+    }).addTo(map);
+
+    //set map boundaries
+    var northW = L.latLng(60, -120);
+    southE = L.latLng(0, -20);
+    var bounds = L.latLngBounds(northW, southE);
+    map.setMaxBounds(bounds);
+    map.on('drag', function () {
+        map.panInsideBounds(bounds, {
+            animate: false
+        });
+    });
 
     //call getData function
     addSharks(map);
@@ -32,6 +69,8 @@ function onEachFeature(feature, layer) {
     };
 };
 
+const basicBeachIcon = L.icon({iconUrl: 'https://raw.githubusercontent.com/shacheeswadia/leaflet-map/main/beach-icon-colorful.svg', iconSize: [25, 25], // size of the icon
+});
 
 function addBeaches(map){
     //load the data
@@ -52,7 +91,7 @@ function addBeaches(map){
             L.geoJson(json, {
                 onEachFeature: onEachFeature,
                 pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                    return L.circleMarker(latlng, {icon: basicBeachIcon});
                 }
             }).addTo(map);
         })
